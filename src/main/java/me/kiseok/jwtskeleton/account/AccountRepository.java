088c2d4f227a192b02b24d244760eca152c52bdd
kiseok, Yang <rltjr219@gmail.com>
2020-10-11 02:23:39 +0900

    Remove Files about Account

--- src/main/java/me/kiseok/jwtskeleton/account/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package me.kiseok.jwtskeleton.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
