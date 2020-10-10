173e9080a07b6b63141013b77afb582b67077864
kiseok, Yang <rltjr219@gmail.com>
2020-10-10 19:20:54 +0900

    first commit

---
+++ src/test/java/me/kiseok/jwtskeleton/JwtSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kiseok.jwtskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
